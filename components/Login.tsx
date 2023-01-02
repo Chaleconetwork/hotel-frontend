d3c7157c184ecd6a6ffa97cc5ca17d0f3e252e9f
chaleconetwork <chaleconetworking@gmail.com>
2023-01-02 14:10:31 -0300

    fetch module

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,11 +1,44 @@
 import styles from '../styles/Login.module.css'
 import Link from 'next/link'
 import { motion } from "framer-motion";
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import Router from 'next/router';
+import { Fetch } from '../utils/api/fetch'
+
+interface iFormData {
+    username: string;
+    password: string;
+}
 
 export default function Login() {
+
+    const [user, setUser] = useState<iFormData>({ username: '', password: '' });
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    async function handleClick(e: any) {
+        e.preventDefault();
+
+        const dto = {
+            username: user.username,
+            password: user.password
+        }
+
+        const url = 'http://localhost:5107/api/Auth/loginUser'
+        const response = await Fetch.postApi(url, dto);
+
+        if (response.isError) {
+            alert(response.messageError);
+            return false;
+        }
+
+        Router.push('/main');
+    }
+
     return (
         <div className={styles.container}>
-            <motion.form action="/main" 
+            <motion.form
                 initial={{ scale: 0 }}
                 animate={{ rotate: 360, scale: 1 }}
                 transition={{
@@ -16,17 +49,17 @@ export default function Login() {
                 <h2>Iniciar Sesión</h2>
                 <div className={styles.formContainer}>
                     <div className={styles.formGroup}>
-                        <input className={styles.formInput} name='user' type="text" placeholder=' ' />
+                        <input className={styles.formInput} type='text' name='username' onChange={handleChange} placeholder=' ' />
                         <label className={styles.formLabel} htmlFor="user">Usuario</label>
                         <span className={styles.formLine}></span>
                     </div>
-                    <div className={styles.formGroup}>                        
-                        <input className={styles.formInput} type="password" placeholder=' ' />
+                    <div className={styles.formGroup}>
+                        <input className={styles.formInput} type='password' name='password' onChange={handleChange} placeholder=' ' />
                         <label className={styles.formLabel} htmlFor="">Contraseña</label>
                         <span className={styles.formLine}></span>
                     </div>
                     <div className={styles.formGroup}>
-                        <input className={styles.formSubmit} type="submit" value='Entrar' />
+                        <input className={styles.formSubmit} type="submit" onClick={handleClick} value='Entrar' />
                     </div>
                 </div>
                 <p>¿Olvidó su contraseña?</p> <Link href="/"><p className={styles.clickaqui}>Click aquí</p></Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,12 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Login from '../components/Login'
-
-const inter = Inter({ subsets: ['latin'] })
+import wave from '../assets/svg/wave.svg'
 
 export default function Home() {
     return (
         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,128L60,112C120,96,240,64,360,80C480,96,600,160,720,165.3C840,171,960,117,1080,101.3C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+            {/* <svg className='svgDesign' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,128L60,112C120,96,240,64,360,80C480,96,600,160,720,165.3C840,171,960,117,1080,101.3C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg> */}
             <Login />
         </div>
     )
